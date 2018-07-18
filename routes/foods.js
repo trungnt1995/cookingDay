34c9044185b7c8883dd30a653a93fee7354cef33
Trung Nguyen <nttrung13091995@gmail.com>
2018-07-18 16:32:58 +0700

    deploy

--- routes/foods.js
+++ routes/foods.js
@@ -4,6 +4,7 @@ var	userHandlers = require('../controllers/userController.js');
 var	foodHandlers = require('../controllers/foodController.js');
 /* GET home page. */
 router.get('/:categoryId',userHandlers.loginRequired, foodHandlers.get);
+// new
 router.get('/:foodById',userHandlers.loginRequired, foodHandlers.getFoodByUser);
 router.post('/:categoryId/create',userHandlers.loginRequired, foodHandlers.create);
 router.post('/:foodId/update',userHandlers.loginRequired, foodHandlers.update);
